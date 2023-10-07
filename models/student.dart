1eabf0690b24111a0282ca1c5a7af57f37ba6cbb
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-10-07 15:00:51 +0300

    🏫

---
+++ models/student.dart
@@ -0,0 +1,12 @@
+/*
+1. Создайте класс Student, который будет представлять студента. Класс должен содержать следующие свойства:
+   - String name - имя студента.
+   - int age - возраст студента.
+   - String department - факультет, на котором учится студент.
+*/
+class Student {
+  late int id;
+  late String name;
+  late int age;
+  late String group;
+}
